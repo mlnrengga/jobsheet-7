9b567cb5ee0eb7961a632ca6418b033e47224d97
mlnrengga <mlnrengga@gmail.com>
2023-10-15 20:42:38 +0700

    Add; Rata-Rata

--- ForKelipatan15.java
+++ ForKelipatan15.java
@@ -5,6 +5,7 @@ Scanner scan = new Scanner (System.in);
 
 int kelipatan;
 int jumlah = 0, counter = 0;
+double rataRata;
 
 System.out.print("Masukkan Bilangan Kelipatan (1-9): ");
 kelipatan = scan.nextInt();
@@ -15,8 +16,10 @@ for (int i = 1; i <= 50; i++) {
         counter++;
     }
 }
+    rataRata = jumlah / counter;
 System.out.printf("Banyaknya Bilangan %d dari 1 Sampai 50 Adalah %d\n", kelipatan, counter);
 System.out.printf("Total Bilangan Kelipatan %d dari 1 Sampai 50 Adalah %d\n", kelipatan, jumlah);
+System.out.printf("Rata Rata Dari Bilangan Kelipatan %d adalah %s", kelipatan, rataRata);
 scan.close();
 }
 }
